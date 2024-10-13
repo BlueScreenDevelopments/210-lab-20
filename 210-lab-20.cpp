57f471f54e41572bf07f081fa2f5df03008233f6
Brad Julao <pogiboybrad@gmail.com>
2024-10-13 11:15:51 -0700

    Commit 10-13 11:15

--- 210-lab-20.cpp
+++ 210-lab-20.cpp
@@ -1,20 +1,91 @@
-// 210-lab-20.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Brandon Julao
+// COMSC-210-5481
+// Lab 20
+// 10-13-2024
 
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+const int SIZE = 3;
+
+class Chair {
+private:
+    int legs;
+    double* prices;
+
+public:
+    // Default constructor
+    Chair() {
+        srand(time(0)); // Seed for random number generation
+        legs = rand() % 2 + 3; // Randomly select 3 or 4 legs
+        prices = new double[SIZE];
+        for (int i = 0; i < SIZE; i++)
+            prices[i] = 100.00 + static_cast<double>(rand()) / RAND_MAX * 899.99; // Randomly select prices between 100.00 and 999.99
+    }
+
+    // Parameter constructor
+    Chair(int l, double p[]) {
+        legs = l;
+        prices = new double[SIZE];
+        for (int i = 0; i < SIZE; i++)
+            prices[i] = p[i];
+    }
+
+    // Setters and getters
+    void setLegs(int l) { legs = l; }
+    int getLegs() { return legs; }
+    void setPrices(double p1, double p2, double p3) {
+        prices[0] = p1; prices[1] = p2; prices[2] = p3;
+    }
+
+    double getAveragePrices() {
+        double sum = 0;
+        for (int i = 0; i < SIZE; i++)
+            sum += prices[i];
+        return sum / SIZE;
+    }
+
+    void print() {
+        cout << "CHAIR DATA - legs: " << legs << endl;
+        cout << "Price history: ";
+        for (int i = 0; i < SIZE; i++)
+            cout << fixed << setprecision(2) << prices[i] << " ";
+        cout << endl << "Historical avg price: " << getAveragePrices() << endl << endl;
+    }
+
+    // Destructor to free allocated memory
+    ~Chair() {
+        delete[] prices;
+    }
+};
+
+int main() {
+    cout << fixed << setprecision(2);
+
+    // Creating pointer to first chair object using default constructor
+    Chair* chairPtr = new Chair;
+    chairPtr->print();
+    delete chairPtr;
+    chairPtr = nullptr;
+
+    // Creating dynamic chair object with parameter constructor
+    double prices[] = { 121.21, 232.32, 414.14 };
+    Chair* livingChair = new Chair(3, prices);
+    livingChair->print();
+    delete livingChair;
+    livingChair = nullptr;
+
+    // Creating dynamic array of chair objects using default constructors
+    Chair* collection = new Chair[SIZE];
+    for (int i = 0; i < SIZE; i++)
+        collection[i].print();
+
+    delete[] collection; // Deallocate memory for the array of Chair objects
+    collection = nullptr;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
